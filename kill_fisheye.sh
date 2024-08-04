2e0f37b2ec0120486cbc01282dc9039541a4bb41
Raunaq Bhirangi <raunaqbhirangi@nyu.edu>
2024-08-04 12:21:34 -0400

    update camera server bash files

--- kill_fisheye.sh
+++ kill_fisheye.sh
@@ -1,3 +1,5 @@
 sudo fuser -k 10010/tcp
 sudo fuser -k 10011/tcp
 sudo fuser -k 10012/tcp
+sudo fuser -k 10061/tcp
+sudo fuser -k 10062/tcp
--- launch_server.sh
+++ launch_server.sh
@@ -1,4 +1,7 @@
-mkdir logs
-nohup python3 robot_camera.py > logs/camera_log.txt &
+#!/bin/bash
+conda activate crossmodal
+export PATH="/home/raunaq/miniconda3/envs/crossmodal/bin:$PATH"
+mkdir -p logs
+nohup /home/raunaq/miniconda3/envs/crossmodal/bin/python robot_camera.py > logs/camera_log.txt &
 cd server
-nohup gunicorn -w 12 -b 0.0.0.0:5000 -k gevent --timeout 0 --worker-connections 2 'monitor:app' > ../logs/cam_server.txt &
+nohup /home/raunaq/miniconda3/envs/crossmodal/bin/gunicorn -w 12 -b 0.0.0.0:5000 -k gevent --timeout 0 --worker-connections 2 'monitor:app' > ../logs/cam_server.txt &
